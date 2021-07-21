e883d254f5ae5fffdb2b10fd365c0f3641791f50
carlops22 <cvenegasa@udec.cl>
2021-07-21 16:36:31 -0400

    Add files via upload

--- MapAVL.cpp
+++ MapAVL.cpp
@@ -1,22 +1,94 @@
 #include "MapAVL.h"
-
+#include <algorithm>
 MapAVL::MapAVL(){
-	
+	root=nullptr;
 }
 
 MapAVL::~MapAVL(){
 	
 }
+void MapAVL::init_nodo(nodo &nodio,pair<string,int> &pair){
+			nodio->par=pair;
+			nodio->padre=nullptr;
+			nodio->izq=nullptr;
+			nodio->der=nullptr;
+			nodio->balance=0;
+		}
 
-void MapAVL::insert(pair<string,int>){
+nodo& MapAVL::bin_searchtree(nodo &nodio, string key){
+		if (nodio == nullptr || key == nodio->par.first) {
+			return nodio;
+		}
+
+		if (key < nodio->par.first) {
+			return searchTreeHelper(nodio->izq, key);
+		} 
+		return searchTreeHelper(nodio->der, key);
+}
+
+nodo& MapAVL::delete_nodo(nodo &nodio, string key){
+	// busca la llave, traversando el arbol
+	if(nodio==nullptr)
+		return nodio;
+	else if(key<nodio->par.first)
+		nodio->izq=delete_nodo(nodio->izq,key);
+	else if(key>nodio->par.first)
+		nodio->der=delete_nodo(nodio->der,key);
+	
+	//encontro la llave, basta borrarlo
+	else{ 
+		//caso 1: nodo es hoja
+		if(nodio->izq==nullptr && nodio->der==nullptr){
+			delete nodio;
+			nodio=nullptr;
+		}
+		
+		//caso 2: nodo tiene un solo hijo
+		else if(nodio->izq== nullptr){
+			nodo *temp=nodio;
+			nodio=nodio->der;
+			delete temp;
+		}
+		
+		else if(nodio->der== nullptr){
+			nodo *temp=nodio;
+			nodio=nodio->izq;
+			delete temp;
+		}
+		
+		//caso 3: tiene ambos hijos (izquierda,derecha)
+		else{
+			nodo *temp=minimum(nodio->der);
+			nodio->par=temp->par;
+			nodio->der= delete_nodo(nodio->der,temp->par.first);
+		}
+	}
+	
+	//ahora se updatea el balance de los nodos, piko idea :) 
+	
+	
+	return nodio;
+}
+
+void MapAVL::update_balance(nodo &nodio){
+	
+	//caso 1: el nodo esta desbalanceado, se prosigue a balancear
+	if(nodio->balance<-1 || nodio->balance>1){
+		rebalance(nodio);
+		return;
+	}
+	
+	
+}
+void MapAVL::insert(pair<string,int> pair){
 	
 }
 
-void MapAVL:: erase(string){
+void MapAVL::erase(string key){
 	
 }
 
-int MapAVL::at(string){
+int MapAVL::at(string key){
 	
 }
 
--- MapAVL.h
+++ MapAVL.h
@@ -1,25 +1,30 @@
 #ifndef MAPAVL_H
 #define MAPAVL_H
-
-
 #include "ADTMap.h"
 #include<vector>
+#include <bits/stdc++.h>
 using namespace std;
 
+struct nodo{
+			nodo *padre;
+			nodo *izq, *der;
+			pair<string,int> par;
+			int balance;
+};
 
 class MapAVL: public ADTMap{
 
 	private:
-		struct nodo{
-			nodo *padre;
-			nodo *izq, *der;
-			int altura;
-			int balance;
-			nodo(pair<string,int> &pair, nodo &p, nodo &i, nodo &d);
-			void update_balance();
-		}
+		nodo *raiz;	
+		void init_nodo(nodo &nodio,pair<string,int> &pair);
+		void preOrder(nodo &nodio);
+		void inOrder(nodo &nodio);
+		void postOrder(nodo &nodio);
+		void bin_searchtree(nodo &nodio, string key);
+		void delete_nodo(nodo &nodio, string key);
+		void update_balance(nodo &nodio);
+		
 	public:
-	
 		MapAVL();
 		~MapAVL();
 		void insert(pair<string,int>);
@@ -27,10 +32,6 @@ class MapAVL: public ADTMap{
 		int at(string);
 		int size();
 		bool empty();	
-
-
-
-
 };
 
 
